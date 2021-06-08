e2d2406e1f6a87b144b7acec1c31556580835f86
steve <steve-li-1998@outlook.com>
2021-06-08 13:34:31 +0200

    a已完成，b暂未开始

--- a3_b.c
+++ a3_b.c
@@ -16,53 +16,30 @@ void *work(void *arg)
     int currentToolsIndex[EXCAVATIONS_PER_ARCHAEOLOGIST];
     for (int i = 0; i < EXCAVATIONS_PER_ARCHAEOLOGIST; ++i) {
         ExcavationStep excavationStep = get_next_step();
-        if (pthread_mutex_lock(&lock)){
-            fprintf(stderr, "pthread_mutex_lock failed");
-            exit(1);
-        }
         if (sem_wait(&tools[excavationStep.toolIndex].sem)){
             fprintf(stderr, "sem_wait failed");
             exit(1);
         }
         currentToolsIndex[i] = excavationStep.toolIndex;
-        if (pthread_mutex_unlock(&lock)){
-            fprintf(stderr, "pthread_mutex_unlock failed");
-            exit(1);
-        }
         sleep(excavationStep.time);
         if (excavationStep.finished){
-            if (pthread_mutex_lock(&lock)){
-                fprintf(stderr, "pthread_mutex_lock failed");
-                exit(1);
-            }
             if(sem_post(&tools[excavationStep.toolIndex].sem)){
                 fprintf(stderr, "sem_post failed");
                 exit(1);
             }
             currentToolsIndex[i] = TOOL_COUNT;
             //da [0...TOOL_COUNT-1] schon besitzt werden. wenn index TOOL_COUNT ist, bedeutet das Werkzeug zurueckgegeben wird
-            if (pthread_mutex_unlock(&lock)){
-                fprintf(stderr, "pthread_mutex_unlock failed");
-                exit(1);
-            }
         }
     }
     for (int i = 0; i < EXCAVATIONS_PER_ARCHAEOLOGIST; ++i) {
         if(currentToolsIndex[i]<TOOL_COUNT){
-            if (pthread_mutex_lock(&lock)){
-                fprintf(stderr, "pthread_mutex_lock failed");
-                exit(1);
-            }
             if(sem_post(&tools[excavationStep.toolIndex].sem)){
                 fprintf(stderr, "sem_post failed");
                 exit(1);
             }
-            if (pthread_mutex_unlock(&lock)){
-                fprintf(stderr, "pthread_mutex_unlock failed");
-                exit(1);
-            }
         }
     }
+}
 
 
 	printf("Alle Ausgrabungen von [A%d] sind fertig\n", my_num);
